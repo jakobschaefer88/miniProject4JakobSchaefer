9d1af4e998533d83a737451289400d614ede768d
jakobschaefer88 <jaschaefer2@mail.fhsu.edu>
2025-04-06 21:01:46 -0500

    updated admin file to show dates/times

--- djangoProject/polls/models.py
+++ djangoProject/polls/models.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.db import models
 from django.utils import timezone
 import datetime
@@ -12,6 +13,16 @@ class Question(models.Model):
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    @admin.display(
+        boolean=True,
+        ordering="pub_date",
+        description="Published recently?",
+    )
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
